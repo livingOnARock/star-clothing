6a7f07a5daf28e2dff6f410fbfe0d6aed1b111e3
livingonarock <livingonarock@mail.com>
2019-07-30 12:55:11 -0500

    added starter react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Homepage from "./pages/homepage/homepage.component";
-import "./App.css";
 
+import Homepage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+let Hats = () => {
+  return <div className="homepage">Hats</div>;
+};
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
