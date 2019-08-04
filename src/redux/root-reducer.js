75a9cc97325e897b5fe6ee571cf47baff13a4bc6
livingonarock <livingonarock@mail.com>
2019-08-04 13:38:06 -0500

    moved directory and shop data into its own reducer and respectivly created redux flow for directory/shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,25 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+// import sessionStorage from "redux-persist/lib/storage"; // if we want session storage
+import storage from "redux-persist/lib/storage"; // if we want local storage
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-export default combineReducers({
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
+
+// json object that represents the possible configurations we want redux-persist to use
+const persistConfig = {
+  key: "root", // key is what point inside our reducer do we want to start storing everything
+  storage,
+  whitelist: ["cart"] // array of string names of any of the reducer that we want to store
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
